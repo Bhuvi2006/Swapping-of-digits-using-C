995a8d31382a2909c9c404864c3677bdf0e2d11a
Bhuvi21 <bhuvijai2006@gmail.com>
2024-07-13 10:25:09 +0530

    Add files via upload

---
+++ Swapping of digits.c
@@ -0,0 +1,16 @@
+
+// Swapping of digits
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("\nEnter the digits for A and B");
+    scanf("%d%d",&a,&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("A: %d B: %d",a,b);
+} 
+ 
+
